49b884b6d8027ee0404dfc14ceb18f64091c42b1
tivvvv <18612553935@163.com>
2026-02-12 08:05:02 +0800

    feat: 流式响应.

--- src/main/java/com/tiv/spring/ai/doctor/controller/OllamaController.java
+++ src/main/java/com/tiv/spring/ai/doctor/controller/OllamaController.java
@@ -1,22 +1,28 @@
 package com.tiv.spring.ai.doctor.controller;
 
 import jakarta.annotation.Resource;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/ollama")
 public class OllamaController {
 
     @Resource
-    OllamaChatClient ollamaChatClient;
+    private OllamaChatClient ollamaChatClient;
 
     @GetMapping("/chat")
     public Object chat(@RequestParam String msg) {
-        return ollamaChatClient.call(msg);
+        Prompt prompt = new Prompt(new UserMessage(msg));
+        Flux<ChatResponse> stream = ollamaChatClient.stream(prompt);
+        return stream;
     }
 
 }
