17948487719c6387b6918144beabbff7a652469e
tivvvv <18612553935@163.com>
2026-02-13 16:19:13 +0800

    feat: 解决SSE服务跨域问题.

---
+++ src/main/java/com/tiv/spring/ai/doctor/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.tiv.spring.ai.doctor.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.domain}")
+    private String domain;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 覆盖所有请求
+        registry.addMapping("/**")
+                // 允许发送Cookie
+                .allowCredentials(true)
+                // .allowedOrigins(domain)
+                // 放行哪些域名(必须用patterns,否则*会和allowCredentials冲突)
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+
+}
